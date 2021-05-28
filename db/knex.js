7ef24491fbcd19ef641befb37ced8fa194e7f4c9
cgdesarrolloapp <cesar.rodriguez@sofoscorp.com>
2021-05-28 18:41:58 -0500

    Reporte radiobase basico

--- db/knex.js
+++ db/knex.js
@@ -4,7 +4,8 @@ const connectedKnex = knex({
     client : "sqlite3",
     connection: {
         filename: "radiobaseDB.sqlite3"
-    }
+    },
+    useNullAsDefault: true
 })
 
 module.exports = connectedKnex
