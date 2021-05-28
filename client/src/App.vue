d6ba376c9861f75b7086a4ce04cff58bf56b54ec
cgdesarrolloapp <cesar.rodriguez@sofoscorp.com>
2021-05-28 18:08:20 -0500

    Radiobase reporte basico

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,45 +1,89 @@
 <template>
   <div id="app">
-        <!--ul id="array-rendering">
-          <li v-for="radiobase in rb" :key="radiobase">
-            {{ radiobase.RADIOBASE }}
-          </li>
-        </ul>
-        <br /-->
-        <br />
-        <text >Ingrese Radiobase</text>
-        <br />
-        <input
+    <form
+    v-bind:style="{ padding: '10px'}"
+    v-on:submit.prevent="onSubmit"
+      id="appForm">
+      <br />
+      <label v-bind:style="{ padding: '10px', margin : '10px'}" >Ingrese Radiobase</label>
+      <br />
+      <input
+        v-bind:style="{ padding: '10px', margin : '10px'}"
         id="inpRadioBase"
-          name="RADIOBASES"
-          placeholder="Radiobase"
-          value="RBZA9931C003"
-        />
-        <br />
-        <text >Seleccione Fecha</text>
-        <br />
-        <input
-          id="dpFecha"
-          type="date"
-          name="FECHA"
-          placeholder="Fecha"
-          value=""
-        />
-        <br />
-        <input type="submit" @click="mounted" />
-   
-    <table class="table mt-5">
+        name="RADIOBASES"
+        placeholder="Radiobase"
+        value="RBZA9931C003"
+      />
+      <br />
+      <label v-bind:style="{ padding: '10px', margin : '10px'}" >Seleccione Fecha</label>
+      <br />
+      <input
+        id="dpFecha"
+        v-bind:style="{ padding: '10px', margin : '10px'}"
+        type="date"
+        name="FECHA"
+        placeholder="Fecha"
+        value=""
+      />
+      <br />
+      <input type="submit" @click="consultarRadioBase" />
+    </form>
+
+    <table id="tblRadio" class="table mt-5">
       <thead>
-        <tr id="rotate"  >
-          <th style="max-width: 100px,  min-width: 100px" scope="col">Radiobases</th>
-          <td :style="{ transform: 'rotate('+ turn+'turn)'}" v-for=" i in Object.keys(aDateReport.fechatrafico)" :key="i"  >{{i}}</td> 
+        <tr id="rotate">
+          <th
+            :style="{
+              width: '90px',
+              minWidth: '90px',
+              height: '140px',
+              border: '1px solid black',
+            }"
+            style="max-width: 100px,  min-width: 100px"
+            scope="col"
+          >
+            Radiobases
+          </th>
+          <td
+            :style="{
+              transform: 'rotate(' + turn + 'turn)',
+              width: '90px',
+              minWidth: '90px',
+              height: '140px',
+              border: '1px solid black',
+            }"
+            v-for="i in Object.keys(aDateReport.fechatrafico)"
+            :key="i"
+          >
+            {{ i }}
+          </td>
         </tr>
       </thead>
-      <tbody>
-        <tr >
-          <th  scope="row">{{radiobases}}</th>
-          <!--td v-for="item  in  Object.keys(aDateReport)" :key="item.RADIOBASE">{{item.TRAFICO}}</td-->
-          <td v-bind:style="{ height: '150px'}"  v-for=" i in Object.values(aDateReport.fechatrafico)" :key="i"  >{{i}}</td> 
+      <tbody :class="tblBody">
+        <tr>
+          <th
+            v-bind:style="{ height: '150px', border: '1px solid black' }"
+            scope="row"
+          >
+            {{ radiobases }}
+          </th>
+          <!--span  v-bind:style="parseInt(i) <= 15 ? coloBG = 'red' : parseInt(i) > 15 && parseInt(i) <= 40 ? coloBG = '#ff8000' : parseInt(i) > 40 && parseInt(i) <= 90 ? colorBg = 'yellow' : parseInt(i) < 90 ? colorBG = 'green' : colorBG = 'grey'"-->
+          <td 
+            v-bind:id="dynamicId++"
+            v-bind:style="
+            {
+              height: '150px',
+              border: '1px solid black',
+              backgroundColor: ColorBG,
+            }"
+
+            v-for="i in Object.values(aDateReport.fechatrafico)"
+            :key="i"
+            
+          >
+            {{ i }}
+          </td>
+
         </tr>
       </tbody>
     </table>
@@ -53,15 +97,32 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-},
+}
+,
+#tblRadio {
+  border: 2px solid black;
+}
+,
+#tblRadio td {
+  border: 2px solid black;
+}
+,
+#tblRadio th {
+  border: 2px solid black;
+}
+,
 #rotate td {
-     -moz-transform: rotate(-90.0deg);  /* FF3.5+ */
-       -o-transform: rotate(-90.0deg);  /* Opera 10.5 */
-  -webkit-transform: rotate(-90.0deg);  /* Saf3.1+, Chrome */
-},
-table, th, td {
+  -moz-transform: rotate(-90deg); /* FF3.5+ */
+  -o-transform: rotate(-90deg); /* Opera 10.5 */
+  -webkit-transform: rotate(-90deg); /* Saf3.1+, Chrome */
+}
+,
+table,
+th,
+td {
   border: 1px solid black;
-}table {
+}
+table {
   border: 2px solid #42b983;
   border-radius: 3px;
   background-color: #fff;
@@ -69,8 +130,8 @@ table, th, td {
 
 th {
   color: black;
-  background-color: #FFFFFF;
-  color: rgba(255,255,255,0.66);
+  background-color: #ffffff;
+  color: rgba(255, 255, 255, 0.66);
   cursor: pointer;
   -webkit-user-select: none;
   -moz-user-select: none;
@@ -83,7 +144,8 @@ td {
   background-color: #f9f9f9;
 }
 
-th, td {
+th,
+td {
   color: black;
   min-width: 120px;
   padding: 10px 20px;
@@ -96,106 +158,135 @@ th.active {
 th.active .arrow {
   opacity: 1;
 }
-
 </style>
 <script>
-import axios from "axios"
-import moment from 'moment'
+import axios from "axios";
+import moment from "moment";
 export default {
-  name: 'App',
-  components: {
-  },
+  name: "App",
+  components: {},
   data() {
     return {
-      items : [],
-      date:"",
-      rb : [],
-      radiobases:"",
-      aDateRange:[],
-      aDateReport:{fechatrafico:{}},
+      items: [],
+      date: "",
+      rb: [],
+      radiobases: "",
+      aDateRange: [],
+      aDateReport: { fechatrafico: {} },
       turn: 0.75,
-      widthTd:50
+      widthTd: 50,
+      ColorBG: "red",
+      IndexId: 0,
+    };
+  },
+  beforemounted() {
+    let oData;
+    let fecha = moment.utc(moment()).endOf("day").format();
+    console.log("fecha", fecha);
+    this.aDateRange.push(moment(fecha).format("YYYY-MM-DD"));
+    for (let step = 0; step < 29; step++) {
+      let diasAtras;
+      diasAtras = moment(fecha)
+        .subtract(step + 1, "days")
+        .format("YYYY-MM-DD");
+      this.aDateRange.push(diasAtras);
     }
- 
+    if (this.items.length == 0) {
+      oData = {
+        RADIOBASE: "",
+        REGION: "",
+        fechatrafico: {},
+      };
+      this.aDateRange.forEach((date) => {
+        oData.fechatrafico[date] = "";
+      });
+    }
+  },
+  mounted() {},
+  updated() {
+    for (let step = 0; step < 29; step++) {
+      //document.getElementById("1").style.backgroundColor = "lightblue";
+    }
   },
   methods: {
-
-      async maestroRadioBases(){
-        const response = await axios.get('http://localhost:5000/maestroRadiobases')
-        console.log("response",response)
-        this.rb = response.data.results
-      },
-    async mounted(){
-      let sDateP = document.getElementById("dpFecha").value
-      let sRadioBaseInp = document.getElementById("inpRadioBase").value
-      this.radiobases = []
-      this.radiobases = sRadioBaseInp
-      this.date = sDateP
-      this.aDateRange = []
-      this.items = []
-      console.log("this.date",this.date)
-      let fecha = moment.utc(this.date).endOf('day').format()
-      console.log("fecha",fecha)
-      this.aDateRange.push(moment(fecha).format('YYYY-MM-DD'))
-      for (let step = 0; step < 10; step++) {
-        let diasAtras 
-        diasAtras = moment(fecha).subtract(step + 1, 'days').format('YYYY-MM-DD')
-        this.aDateRange.push(diasAtras)
+    colorBack(val) {
+      console.log("aaaaaaa", val);
+      return "red";
+    },
+    async maestroRadioBases() {
+      const response = await axios.get(
+        "http://localhost:5000/maestroRadiobases"
+      );
+      console.log("response", response);
+      this.rb = response.data.results;
+    },
+    async consultarRadioBase() {
+     // document.getElementById(".tblBody > td") ? document.getElementById(".tblBody > td").remove : document.getElementById(".tblBody > td")
+      let sDateP = document.getElementById("dpFecha").value;
+      let sRadioBaseInp = document.getElementById("inpRadioBase").value;
+      this.radiobases = [];
+      this.radiobases = sRadioBaseInp;
+      this.date = sDateP;
+      this.aDateRange = [];
+      this.items = [];
+      console.log("this.date", this.date);
+      let fecha = moment.utc(this.date).endOf("day").format();
+      console.log("fecha", fecha);
+      this.aDateRange.push(moment(fecha).format("YYYY-MM-DD"));
+      for (let step = 0; step < 29; step++) {
+        let diasAtras;
+        diasAtras = moment(fecha)
+          .subtract(step + 1, "days")
+          .format("YYYY-MM-DD");
+        this.aDateRange.push(diasAtras);
       }
-      this.aDateRange = this.aDateRange.sort().reverse()
-      console.log("this.aDateRange",this.aDateRange)
-      const response = await axios.get('http://localhost:5000/reporteRadiobases?RADIOBASE='+this.radiobases+'&FECHA='+this.date)
-      console.log("response",response)
-      var oData
-      this.items = response.data.results
-       this.items.forEach(item => {
-                   oData = {
-                          RADIOBASE : item.RADIOBASE,                                    
-                          REGION : item.REGION,
-                          fechatrafico:{}
-                        }
-        /*this.aDateRange.find(element => {
-          console.log("element yyyy", element)
-          console.log("item.FECHA yyyy", item.FECHA)
-          console.log("item yyyy", item.TRAFICO)
-          if(item.FECHA !== element){
-                  oData.fechatrafico[element] = "0"
-          }else{
-                   oData.fechatrafico[item.FECHA] = item.TRAFICO
+      this.aDateRange = this.aDateRange.sort().reverse();
+      console.log("this.aDateRange", this.aDateRange);
+      const response = await axios.get(
+        "http://localhost:5000/reporteRadiobases?RADIOBASE=" +
+          this.radiobases +
+          "&FECHA=" +
+          this.date
+      );
+      console.log("response", response);
+      var oData;
+      this.items = response.data.results;
+      this.items.forEach((item) => {
+        oData = {
+          RADIOBASE: item.RADIOBASE,
+          REGION: item.REGION,
+          fechatrafico: {},
+        };
+        this.aDateRange.forEach((date) => {
+          const result = this.items.filter((item1) => item1.FECHA == date);
+          console.log("000 zzzzz", result);
+          if (result.length > 0) {
+            oData.fechatrafico[date] = result[0].TRAFICO;
+          } else {
+            if (!oData.fechatrafico[date]) {
+              oData.fechatrafico[date] = "";
+            }
           }
-          console.log("oData",oData)
-    
-        })*/
+        });
+        console.log("oData", oData);
+      });
+      if (this.items.length == 0) {
+        oData = {
+          RADIOBASE: "",
+          REGION: "",
+          fechatrafico: {},
+        };
+        this.aDateRange.forEach((date) => {
+          oData.fechatrafico[date] = "";
+        });
+      }
 
-        this.aDateRange.forEach(date => {
-            const result =  this.items.filter(item1 => item1.FECHA == date);
-            console.log("zzzzz", result)
-
-
-          if(result.length > 0){
-            oData.fechatrafico[date] = result[0].TRAFICO
-          }else{
-            if (!oData.fechatrafico[date] ){
-                  oData.fechatrafico[date] = "0"
-            } 
-          }
-        })
-
-        
-
-                console.log("oData",oData)
-
-      })
-
-      console.log("oData Final",oData)
-      //this.aDateReport = oData.fechatrafico
-      this.aDateReport = oData
-
-      console.log("Values",Object.values(this.aDateReport.fechatrafico) )
-
-    }  
-  }
-}
+      console.log("oData Final", oData.fechatrafico);
+      this.aDateReport = oData;
+      console.log("Values", Object.values(this.aDateReport.fechatrafico));
+    },
+  },
+};
 </script>
 
 
--- db/radiobasesReport.js
+++ db/radiobasesReport.js
@@ -1,4 +1,5 @@
 const knex = require("./knex")
+const moment = require("moment")
 
 function getAllRadiobaseReports(){
     return knex('radiobasesDB').select('*').where('FECHA', '<', '2019-09-08')
@@ -12,9 +13,16 @@ function getMaestroRadiobase(req, res){
     return knex('customers').distinct('RADIOBASE')
 }
 
-function getRadiobase(req, res){
-    console.log("aaaaaaaaa",req.query)
-    return knex('radiobasesDB').select('*').where('RADIOBASE', 'LIKE', req.query.RADIOBASE, 'AND', 'FECHA','LIKE', req.query.FECHA)
+async function getRadiobase(req, res){
+    console.log("getRadiobase ---",req.query)
+    let fecha = moment.utc(req.query.FECHA).endOf('day').format()
+    let fecha1 = moment(fecha).format('YYYY-MM-DD')
+    console.log("getRadiobase --", fecha1)
+    var Dias = 30
+    let fechaTo = moment(fecha).subtract(Dias, 'days').format('YYYY-MM-DD')
+    console.log(fecha1 +" getRadiobase - "+fechaTo)
+    //return knex('radiobasesDB').select('*').where('RADIOBASE', 'LIKE', req.query.RADIOBASE, 'AND', 'FECHA','LIKE', fecha)
+    return knex('radiobasesDB').select('*').where('RADIOBASE', 'LIKE', req.query.RADIOBASE).whereBetween('FECHA', [fechaTo, fecha1])
 }
 
 module.exports = {
