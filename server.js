d0c779a4c3c16eef8c07dbc7130cc9f81b448ce3
cgdesarrolloapp <cesar.rodriguez@sofoscorp.com>
2021-05-30 09:24:23 -0500

    se quito /dist del gitignore

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, './client/dist')));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './client/src/vistaReporteRadioBase.html'));
+    res.sendFile(path.join(__dirname, './client/dist/'));
   });
 
 //app.get('/', (req, res) => res.send('backend'))
