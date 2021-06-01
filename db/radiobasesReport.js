536f2d5c2c1edcbbb0066b52d2fd9454e2a59f74
cgdesarrolloapp <cesar.rodriguez@sofoscorp.com>
2021-05-31 20:35:09 -0500

    consulta

--- db/radiobasesReport.js
+++ db/radiobasesReport.js
@@ -18,15 +18,16 @@ function getMaestroRadiobase(req, res){
 
 async function getRadiobase(req, res){
     console.log("getRadiobase ---",req.query)
-    let fecha, radiobase 
+    let fecha, radiobase, region 
     req.query.RADIOBASE ? radiobase = req.query.RADIOBASE : radiobase = ''
+    req.query.REGION ? region = req.query.REGION : region = ''
     req.query.FECHA ? fecha = moment.utc(req.query.FECHA).endOf('day').format('YYYY-MM-DD') : fecha = moment.utc(moment()).endOf('day').format('YYYY-MM-DD')
     var Dias = 30
     let fechaTo = moment(fecha).subtract(Dias, 'days').format('YYYY-MM-DD')
     console.log(fecha +" getRadiobase - "+fechaTo)
     //return knex('radiobasesDB').select('*').where('RADIOBASE', 'LIKE', req.query.RADIOBASE, 'AND', 'FECHA','LIKE', fecha)
-    if(radiobase) { 
-        return knex('radiobasesDB').select('*').where('RADIOBASE', '=', radiobase).whereBetween('FECHA', [fechaTo, fecha]) 
+    if(radiobase || region) { 
+        return knex('radiobasesDB').select('*').where('RADIOBASE', '=', radiobase, 'and', REGION, '=', region, ).whereBetween('FECHA', [fechaTo, fecha]) 
     }else{ 
         return knex('radiobasesDB').select('*').whereBetween('FECHA', [fechaTo, fecha])
     }
